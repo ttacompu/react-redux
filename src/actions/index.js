7fbf3d452f580892a4a5524c1d80e804a89ea61e
ttacompu@gmail.com <taung@cgsh.com>
2019-03-11 15:58:12 -0400

    create list

--- src/actions/index.js
+++ src/actions/index.js
@@ -53,8 +53,12 @@ export const addTodo = (text) => (dispatch) => {
     filter
 })*/
 
-export const toggleTodo = (id) => ({
-    type: 'TOGGLE_TODO',
-    id
-})
+export const toggleTodo = (id) => (dispatch) =>{
+    api.toggleTodo(id).then((response)=>{
+            dispatch({
+                type : 'TOGGLE_TODO_SUCCESS',
+                response : normalize(response, schema.todo)
+            })
+    })
+}
 
